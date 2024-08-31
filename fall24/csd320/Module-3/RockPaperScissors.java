1da2c91fa494714bbd15029667ea1331bfc38246
paulromer12 <paulromer2036@gmail.com>
2024-08-31 17:19:30 -0500

    added comments to rock paper scissors

--- fall24/csd320/Module-3/RockPaperScissors.java
+++ fall24/csd320/Module-3/RockPaperScissors.java
@@ -6,14 +6,17 @@ import java.util.Scanner;
 
 public class RockPaperScissors { 
     public static void main (String[] args) {
+        // Initiate scanner and random objects
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-
+        
+        // Array to map choices to number, used in results output
         String choices[] = {"Rock", "Paper", "Scissors"};
 
-        int computerSelection = random.nextInt(3) + 1;
-        int userSelection = 0;
+        int computerSelection = random.nextInt(3) + 1; // assign random value between 1 and 3 for computer's selection of rock, paper, or scissors
+        int userSelection = 0; // initiate user selection variable
 
+        // start game printing a greeting and rules to the console
         System.out.println();
         System.out.println("Let's play Rock-Paper-Scissors!");
         
@@ -22,7 +25,8 @@ public class RockPaperScissors {
         System.out.println("Scissors is 3");
         System.out.println();
 
-
+        // have user select rock, paper, or scissors by typing 1, 2, or 3
+        // only accepts 1, 2, or 3 which will exit the loop and assign input to user selection
         while (true) {
             try {
                 System.out.print("Enter a number between 1 - 3: ");
@@ -37,9 +41,11 @@ public class RockPaperScissors {
             }
         }
         
+        // starts results output
         System.out.println();
         System.out.println("You chose " + choices[userSelection-1] + " and the computer chose " + choices[computerSelection-1] + "!");
 
+        // if statement to print result of the duel
         if ((userSelection == 1 && computerSelection == 3) || (userSelection == 2 && computerSelection == 1) || (userSelection == 3 && computerSelection == 2)) {
             System.out.println("--You win!--");
         } else if ((computerSelection == 1 && userSelection == 3) || (computerSelection == 2 && userSelection == 1) || (computerSelection == 3 && userSelection == 2)) {
--- fall24/csd320/Module-4/Substring.java
+++ fall24/csd320/Module-4/Substring.java
@@ -1,8 +1,6 @@
 //Assignment:
 //Write a program that prompts a user to enter two strings and then checks the strings entered and reports if either the first string is a substring of the second string, or if the second string is a substring of the first string.
 
-package csd320.Module-4;
-
 public class Substring {
     
 }
