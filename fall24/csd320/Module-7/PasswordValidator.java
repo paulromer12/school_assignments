0922ae2cad3f39b36fba1fe58c92fcad43c6502a
paulromer12 <paulromer2036@gmail.com>
2024-09-15 12:26:52 -0500

    created module 7 directory and statred Module-7 assignment

---
+++ fall24/csd320/Module-7/PasswordValidator.java
@@ -0,0 +1,11 @@
+// Some Websites impose certain rules for passwords. Write a method that checks whether a user's input String is a valid password. Suppose the password rules are as follows:
+// 1. A password must have at least eight characters.
+// 2. A password must contain both letters and digits.
+// 3. A password must contain at least one uppercase character.
+// 4. A password must contain at least one lowercase character.
+
+public class PasswordValidator {
+    public static void main (String[] args) {
+        
+    }
+}
