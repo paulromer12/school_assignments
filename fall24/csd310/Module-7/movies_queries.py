0dc67fa896d524c432375c7985b5cd38a1dd0da0
paulromer12 <paulromer2036@gmail.com>
2024-09-22 20:53:47 -0500

    finished movie_queries.py

--- fall24/csd310/Module-7/movies_queries.py
+++ fall24/csd310/Module-7/movies_queries.py
@@ -1,3 +1,30 @@
+# Paul Romer, CSD310, Module 7 assignment, September 22 2024
+
+# for employee in employees:
+# print(“First Name: {}\n Last Name:{}\n Email:{}\n”.format(employee[0], employee[1], employee[2]))
+
+
+# Write four queries, in one Python file.The output from your queries should match the example below, including descriptions of output and format.
+# The first and second query is to select all the fields for the studio and genre tables.
+# The third query is to select the movie names for those movies that have a run time of less than two hours.
+# The fourth query is to get a list of film names, and directors grouped by director.
+
+# -- DISPLAYING Studio RECORDS --
+# Studio ID:
+# Studio Name:
+
+# -- DISPLAYING Genre RECORDS --
+# Genre ID: 
+# Genre Name: 
+
+# -- DISPLAYING Short Film RECORDS --
+# Film Name: 
+# Runtime: 
+
+# -- DISPLAYING Director RECORDS in Order --
+# Film Name: 
+# Director: 
+
 import mysql.connector
 from mysql.connector import errorcode 
 
@@ -24,26 +51,48 @@ except mysql.connector.Error as err:
     else:
         print(err)
 
-finally:
-    db.close()
 
-# Write four queries, in one Python file.The output from your queries should match the example below, including descriptions of output and format.
-# The first and second query is to select all the fields for the studio and genre tables.
-# The third query is to select the movie names for those movies that have a run time of less than two hours.
-# The fourth query is to get a list of film names, and directors grouped by director.
+cursor = db.cursor()
+# cursor.execute("SELECT * FROM film")
+# films = cursor.fetchall()
+# print(films)
 
--- DISPLAYING Studio RECORDS --
-Studio ID:
-Studio Name:
+# First query: Select all fields for the studio table
+print("\n-- DISPLAYING Studio RECORDS --")
+query_studio = "SELECT * FROM studio"
+cursor.execute(query_studio)
+studios = cursor.fetchall()
+for studio in studios:
+    print(f"Studio ID: {studio[0]}\nStudio Name: {studio[1]}\n")
 
--- DISPLAYING Genre RECORDS --
-Genre ID: 
-Genre Name: 
+# Second query: Select all fields for the genre table
+print("\n-- DISPLAYING Genre RECORDS --")
+query_genre = "SELECT * FROM genre"
+cursor.execute(query_genre)
+genres = cursor.fetchall()
+for genre in genres:
+    print(f"Genre ID: {genre[0]}\nGenre Name: {genre[1]}\n")
 
--- DISPLAYING Short Film RECORDS --
-Film Name: 
-Runtime: 
+# Third query: Select movie names with runtime less than two hours (120 minutes)
+print("\n-- DISPLAYING Short Film RECORDS --")
+query_short_films = "SELECT film_name, film_runtime FROM film WHERE film_runtime < 120"
+cursor.execute(query_short_films)
+short_films = cursor.fetchall()
+for film in short_films:
+    print(f"Film Name: {film[0]}\nRuntime: {film[1]} minutes\n")
 
--- DISPLAYING Director RECORDS in Order --
-Film Name: 
-Director: 
+# Fourth query: Select film names and directors, grouped by director
+print("\n-- DISPLAYING Director RECORDS in Order --")
+query_directors = """
+    SELECT film_name, film_director
+    FROM film
+    GROUP BY film_director, film_name
+    ORDER BY film_director
+"""
+cursor.execute(query_directors)
+films_by_director = cursor.fetchall()
+for film in films_by_director:
+    print(f"Film Name: {film[0]}\nDirector: {film[1]}\n")
+
+# Close the connection
+db.close()
