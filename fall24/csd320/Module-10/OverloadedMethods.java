a695a0ce4dc93cdd2fde1d227880ac721d146e0f
paulromer12 <paulromer2036@gmail.com>
2024-09-29 20:14:12 -0500

    created module 9 and 10 csd320

---
+++ fall24/csd320/Module-10/OverloadedMethods.java
@@ -0,0 +1,12 @@
+// Write four overloaded methods that return the average of an array with the following headers:
+// public static short average(short [] array)
+// public static int average(int [] array)
+// public static long average(long [] array)
+// public static double average(double [] array)
+// Write a test program that invokes each of these methods and then displays the average value returned along with a display of the original array elements. Ensure the display is easy to read and understandable. Lastly, each array sent into the methods must be of different sizes to ensure the method code is correctly written.
+
+package fall24.csd320.Module-10;
+
+public class OverloadedMethods {
+    
+}
---
+++ fall24/csd320/Module-9/ArrayTest.java
@@ -0,0 +1,12 @@
+// Write a program that reads and fills an array with 20 integers. Then, using the array elements, calculate the data to find and display:
+// The highest value entered.
+// The lowest value entered.
+// The average of the numbers entered.
+// The sum of the numbers entered.
+// Display this information in a readable display making the data easy to understand.
+
+package fall24.csd320.Module-9;
+
+public class ArrayTest {
+    
+}
