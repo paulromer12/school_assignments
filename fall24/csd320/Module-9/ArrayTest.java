1cce9d426bee15df27f553a805f166a7c2e670b0
paulromer12 <paulromer2036@gmail.com>
2024-09-29 22:52:45 -0500

    array testing in Module-9 ArrayTest.java

--- fall24/csd320/Module-9/ArrayTest.java
+++ fall24/csd320/Module-9/ArrayTest.java
@@ -5,8 +5,29 @@
 // The sum of the numbers entered.
 // Display this information in a readable display making the data easy to understand.
 
-package fall24.csd320.Module-9;
-
 public class ArrayTest {
-    
+    public static void main (String[] args) {
+        
+        // Initialize an array of 20 integers
+        int[] numbers = new int[20];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 101); // Generate a random number
+        }
+        
+        System.out.print("Stringification of the numbers int array: ");
+        System.out.println(numbers);
+        System.out.print("Array: ");
+        printArray(numbers);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
 }
