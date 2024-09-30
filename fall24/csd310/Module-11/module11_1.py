e82c63644fb02802b7bbfda47cb19b0556ddc1d9
paulromer12 <paulromer2036@gmail.com>
2024-09-29 20:13:36 -0500

    created module 9 and 10 csd320

--- fall24/csd310/Module-11/module11_1.py
+++ fall24/csd310/Module-11/module11_1.py
@@ -3,3 +3,37 @@
 # Julie Sakai
 # Paul Romer
 # Tazmin Somerville 
+
+import mysql.connector
+from mysql.connector import errorcode 
+
+config = {
+    'user': 'root',
+    'password': 'Your Password',
+    'host': '127.0.0.1',
+    'database': 'bacchus',
+    'raise_on_warnings': True,
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config['user'], config['host'], config['database']))
+    input("\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+    else:
+        print(err)
+
+
+cursor = db.cursor()
+
+
+
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
