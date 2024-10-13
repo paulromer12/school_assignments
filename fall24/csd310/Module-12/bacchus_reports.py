50d40c6ff4607fc324b4b19d7668db97225278bb
paulromer12 <paulromer2036@gmail.com>
2024-10-12 22:05:59 -0500

    updated print_table() to update formatting, added calculations for sql columns that don't exist anymore

--- fall24/csd310/Module-12/bacchus_reports.py
+++ fall24/csd310/Module-12/bacchus_reports.py
@@ -1,4 +1,4 @@
-# Group 5, Milestone 3, CSD310
+# Group 5, Milestone 5, CSD310
 # William Renard
 # Julie Sakai
 # Paul Romer
@@ -16,74 +16,113 @@ config = {
     'raise_on_warnings': True,
 }
 
+def print_table(headers, data, column_widths=None):
+    if column_widths is None:
+        column_widths = [max(len(str(row[i])) for row in data + [headers]) for i in range(len(headers))]
+    
+    header_format = " | ".join(f"{{:{width}}}" for width in column_widths)
+    row_format = " | ".join(f"{{:<{width}}}" for width in column_widths)
+    
+    print(header_format.format(*headers))
+    print("-" * (sum(column_widths) + 3 * (len(headers) - 1)))
+    
+    for row in data:
+        print(row_format.format(*[str(item) for item in row]))
 
 def get_late_deliveries():
     cursor.execute(
-        "SELECT d.*, s.supplier_name, s.supply_type FROM delivery d JOIN supplier s ON d.supplier_id = s.supplier_id ORDER BY d.delivery_status;")
+        "SELECT d.*, s.supplier_name, s.supply_type FROM Delivery d JOIN Supplier s ON d.supplier_id = s.supplier_id;")
 
     print("\n-- DELIVERY REPORT --")
     print("*This report generates delivery data such as order status and which orders are delayed by how many days.*\n")
-    late_deliveries = cursor.fetchall()
+    deliveries = cursor.fetchall()
 
-    # length is 8
-    for delivery in late_deliveries:
-        if delivery[4] == "Delayed":
-            actual_date = datetime.strptime(str(delivery[3]), '%Y-%m-%d')
-            expected_date = datetime.strptime(str(delivery[2]), '%Y-%m-%d')
-            difference = actual_date - expected_date
-            print("[{}] Delivery from: {} | {} Day(s) Late".format(delivery[4], delivery[6], str(difference.days)))
+    report_data = []
+    for delivery in deliveries:
+        delivery_id, supplier_id, expected_date, actual_date, tracking_number, supplier_name, supply_type = delivery
+        
+        if actual_date is None:
+            status = "Pending"
+            delay = "N/A"
         else:
-            print("[{}] Delivery from: {}".format(delivery[4], delivery[6]))
+            expected = datetime.strptime(str(expected_date), '%Y-%m-%d')
+            actual = datetime.strptime(str(actual_date), '%Y-%m-%d')
+            difference = actual - expected
+            
+            if difference.days > 0:
+                status = "Delayed"
+                delay = f"{difference.days} day(s)"
+            else:
+                status = "On Time"
+                delay = "0 days"
 
+        report_data.append([delivery_id, supplier_name, status, expected_date, actual_date or "N/A", delay])
+
+    headers = ["Delivery ID", "Supplier", "Status", "Expected Date", "Actual Date", "Delay"]
+    print_table(headers, report_data)
 
 def get_employee_report():
     cursor.execute(
-        "SELECT e.employee_ID, e.employee_first_name, w.hours_worked FROM workhours w JOIN employee e ON w.employee_ID = e.employee_ID ORDER BY w.hours_worked DESC;")
+        "SELECT e.employee_ID, e.employee_first_name, e.employee_last_name, w.hours_worked, w.work_date FROM WorkHours w JOIN Employee e ON w.employee_ID = e.employee_ID ORDER BY w.hours_worked DESC;")
 
     print("\n-- EMPLOYEE REPORT --")
-    print(
-        "*This report generates hour data such as how many hours each employee worked for the day and which ones are \n projected to hit overtime and by how much overtime.*\n")
+    print("*This report generates hour data such as how many hours each employee worked for the day and which ones are projected to hit overtime and by how much overtime.*\n")
     employee_hours = cursor.fetchall()
 
+    report_data = []
     for employee in employee_hours:
-        if employee[2] > 8:
-            projection = employee[2] - 8
-            print("Employee: {} [ID: {}] Hours Worked: {} | Projected Overtime: {}".format(employee[1], employee[0],
-                                                                                           employee[2], projection))
+        emp_id, first_name, last_name, hours_worked, work_date = employee
+        if hours_worked > 8:
+            overtime = hours_worked - 8
+            status = f"Overtime: {overtime:.2f} hours"
         else:
-            print("Employee: {} [ID: {}] Hours Worked: {}".format(employee[1], employee[0], employee[2]))
+            status = "Regular"
+        
+        report_data.append([emp_id, f"{first_name} {last_name}", work_date, f"{hours_worked:.2f}", status])
 
+    headers = ["Employee ID", "Name", "Date", "Hours Worked", "Status"]
+    print_table(headers, report_data)
 
 def get_wine_report():
     cursor.execute(
-        "SELECT o.order_id, o.quantity_ordered, w.wine_type, w.wine_name, d.distributor_name FROM `order` o JOIN `wine` w ON o.wine_ID = w.wine_ID JOIN `distributor` d ON o.distributor_id = d.distributor_id;")
+        "SELECT o.order_id, o.quantity_ordered, w.wine_type, w.wine_name, d.distributor_name FROM `Order` o JOIN Wine w ON o.wine_ID = w.wine_ID JOIN Distributor d ON o.distributor_id = d.distributor_id;")
 
     print("\n-- WINE PERFORMANCE REPORT --")
-    print("*This report generates order data based on wine, wine type, and breaks down the total amount of orders based \n on type of wine carried by Bacchus Winery.*")
-    total_red_wine = 0
-    total_white_wine = 0
-    red_wine_varieties = 0
-    white_wine_varieties = 0
-
+    print("*This report generates order data based on wine, wine type, and breaks down the total amount of orders based on type of wine carried by Bacchus Winery.*\n")
+    
     wine_data = cursor.fetchall()
 
+    report_data = []
+    total_red_wine = 0
+    total_white_wine = 0
+    red_wine_varieties = set()
+    white_wine_varieties = set()
+
     for wine in wine_data:
-        if wine[2] == "Red":
-            total_red_wine += int(wine[1])
-            red_wine_varieties += 1
+        order_id, quantity, wine_type, wine_name, distributor = wine
+        report_data.append([order_id, wine_name, wine_type, quantity, distributor])
+        
+        if wine_type == "Red":
+            total_red_wine += quantity
+            red_wine_varieties.add(wine_name)
         else:
-            total_white_wine += int(wine[1])
-            white_wine_varieties += 1
-        print("Wine: {} [{}] | Amount Ordered: {} by Distributor: {}".format(wine[3], wine[2], wine[1], wine[4]))
+            total_white_wine += quantity
+            white_wine_varieties.add(wine_name)
+
+    headers = ["Order ID", "Wine Name", "Type", "Quantity", "Distributor"]
+    print_table(headers, report_data)
 
     print("\n-- TOTAL SALES -- ")
-    print("Sold {} Red bottles of wine in {} variation(s).".format(total_red_wine, red_wine_varieties))
-    print("Sold {} White bottles of wine in {} variation(s).".format(total_white_wine, white_wine_varieties))
+    summary_data = [
+        ["Red", total_red_wine, len(red_wine_varieties)],
+        ["White", total_white_wine, len(white_wine_varieties)]
+    ]
+    summary_headers = ["Wine Type", "Total Bottles Sold", "Number of Varieties"]
+    print_table(summary_headers, summary_data)
 
 try:
     db = mysql.connector.connect(**config)
-    print("\n Database user {} connected to MySQL on host {} with database {}".format(config['user'], config['host'],
-                                                                                      config['database']))
+    print(f"\n Database user {config['user']} connected to MySQL on host {config['host']} with database {config['database']}")
     input("\n Press any key to generate reports...")
 
     cursor = db.cursor()
@@ -94,7 +133,7 @@ try:
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("   The specified database does not exist")
+        print("   The supplied username or password are invalid")
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
         print("   The specified database does not exist")
     else:
