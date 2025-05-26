aa8d0f3fb599b00649610ed276cf8108bbd1162a
paulromer12 <paulromer2036@gmail.com>
2025-05-26 16:20:34 -0500

    CSD440 Module1

---
+++ summer25/csd440/module1/PaulFirstProgram.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Paul's First Program</title>
+</head>
+<body>
+    <h1>CSD440 Module 1 Assignment</h1>
+    <?php
+    // PHP snipit to display text to the screen
+    echo "This is a simple program for the first assignment of CSD440 as part of Module 1.";
+    ?>
+</body>
+</html>
